1f6e382298495d4f373a1fae1a689cdcb87255f2
Aldair Cosetito Coral <aldair.soft@hotmail.com>
2021-01-04 09:04:00 -0500

    Initial Commit

---
+++ Counter-Vipper-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter-Vipper-Swift
+//
+//  Created by Aldair Raul Cosetito Coral on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
